6c5b1d7833f81a6359516582f380c914cc60e15c
Oleksandr <olekhy@googlemail.com>
2013-09-25 00:24:46 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,4 @@ include_once(".config/SESSCFG.php");
 $PORTAL["base"] = preg_replace("/^(.*)\/index\.php$/","$1",$_SERVER["SCRIPT_NAME"]);
 $PORTAL["PROCESS_DOCUMENT"] = true;
 include_once('inc/common.php');
-/*
-lksmäiepfvm ewäf+ewq,vmvcv.cc,v
-*/
 ?>
